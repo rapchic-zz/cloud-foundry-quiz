144318f53940a68bff9da703e7b03e73ed6d5485
Debjani Chatterjee <debjani93@in.ibm.com>
2020-10-10 17:57:16 +0530

    v1

---
+++ README.md
@@ -0,0 +1,27 @@
+# Deploy simple Quiz application and take the quiz
+
+<a href="https://cloud.ibm.com/devops/setup/deploy?repository=https://github.com/rapchic/cloud-foundry-quiz.git&branch=master"><img src="https://cloud.ibm.com/devops/setup/deploy/button.png" alt="Deploy to IBM Cloud"></a>
+
+1. Press the above `Deploy to IBM Cloud` button and then...
+
+   * Click `Create+` to create an IBM Cloud API Key.
+   * Select your region, organization, and space (or use the defaults).
+   * Click `Deploy`.
+
+2. In Toolchains, click on `Delivery Pipeline` to watch while the app is deployed.
+
+   ![toolchain-pipeline](doc/images/tool.png)
+
+3. Click on `View Console` in the Deploy stage
+
+   ![toolchain-pipeline](doc/images/1.png)
+
+4. Once deployed, the app can be viewed by clicking `Visit App URL`
+
+   ![toolchain-pipeline](doc/images/2.png)
+
+5. Attempt the Quiz in the deployed app and submit the response to view your score!
+
+   ![toolchain-pipeline](doc/images/3.png)
+
+hello
---
+++ app.js
@@ -0,0 +1,88 @@
+require('dotenv').config();
+var cloudant = require('cloudant');
+var express = require('express');
+var bodyParser = require('body-parser');
+var stringSimilarity = require('string-similarity');
+let secureEnv = require('secure-env');
+global.env = secureEnv({secret:'mySecretPassword'});
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/public'));
+app.get('/',function(req,res){
+  res.sendFile("views/index.html", {"root": __dirname});
+});
+var cloudant = cloudant({account:global.env.cloudantusername, password:global.env.cloudantpassword});
+
+var responsequiz1;
+var responsequiz2;
+var responsequiz3;
+var responsequiz4;
+var responsequiz5;
+var area_interests;
+var org_name;
+var time;
+var url;
+var counter=0;
+
+quizdb = cloudant.db.use(global.env.dbname);
+
+
+app.post('/quizsubmission', function(req,res){
+    
+    if(stringSimilarity.compareTwoStrings(req.body.quiz_1,'b')){
+       counter++;
+       }
+    if(stringSimilarity.compareTwoStrings(req.body.quiz_2,'a')){
+        counter++;
+    }
+    if(stringSimilarity.compareTwoStrings(req.body.quiz_3,'b')){
+       counter++;
+       }
+    if(stringSimilarity.compareTwoStrings(req.body.quiz_4,'b')){
+        counter++;
+    }
+    if(stringSimilarity.compareTwoStrings(req.body.quiz_5,'a')){
+       counter++;
+       }
+       
+                                      
+    var quizscore = counter;
+    var doc={
+        _id:req.headers.host + req.url,
+        responsequiz1: req.body.quiz_1,
+        responsequiz2: req.body.quiz_2,
+        responsequiz3: req.body.quiz_3,      
+        responsequiz4: req.body.quiz_4,
+        responsequiz5: req.body.quiz_5,
+        quizscore:quizscore,
+        time: new Date().toISOString(),
+        email: req.body.cloudemail,
+        interested: req.body.interested
+        };
+    
+quizdb.insert(doc,function(err,body,header){
+    if(err){
+        res.sendFile(__dirname + "/views/error.html");
+        console.log('Error:'+err.message);
+        return;
+    }
+    else{
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<center><h2>Thank you for taking the quiz!</h2><center>'); 
+    res.write('<center><h3>You have scored '+quizscore+'!</h3><center>');  
+    
+    res.end();
+    }
+});
+    
+    //return res.sendFile(__dirname+"/views/success.html");
+});
+
+const port = 3001;
+app.listen(port, function () {
+    console.log("Server running on port: %d", port);
+});
+module.exports = app;
